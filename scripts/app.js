6d660c076aa4004142fec868607a460939fbdb1b
ChristmasLover12345 <madnes0205@gmail.com>
2025-01-16 14:58:45 -0800

    done with story and single player select mode, also fixed a bug with the local storage

--- scripts/app.js
+++ scripts/app.js
@@ -4,19 +4,30 @@ let gameModeSelectLoad = document.getElementById("gameModeSelectLoad")
 
 
 // Sound effect volume slider
-let savedVolume = localStorage.getItem('audioVolume');
-if (savedVolume !== null)
-{
-    gameModeSelectLoad.volume = parseFloat(savedVolume)
-    volumeControl.value = savedVolume;
-}
-else
-{
-    gameModeSelectLoad.volume = 0.5;
+
+let setVolume = () => {
+    let savedVolume = localStorage.getItem('audioVolume');
+    if (savedVolume !== null)
+    {
+        if(gameModeSelectLoad)
+        {
+        gameModeSelectLoad.volume = parseFloat(savedVolume)}
+        volumeControl.value = savedVolume;
+    
+    }
+    else
+    {
+        gameModeSelectLoad.volume = 0.5;
+    }
 }
 
 volumeControl.addEventListener('input', (event) =>{
     let volume = event.target.value;
+    if(gameModeSelectLoad)
+    {
     gameModeSelectLoad.volume = volume;
+    }
     localStorage.setItem('audioVolume', volume);
 })
+
+setVolume()
